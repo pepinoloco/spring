cc8786ebad5b73a377688414fa0fb2474f54644c
pepinoloco <pepinoloco@users.noreply.github.com>
2024-12-20 19:58:43 -0500

    Taco Cloud Application Intial commit

--- src/test/java/tacos/TacoCloudApplicationTests.java
+++ src/test/java/tacos/TacoCloudApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package tacos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class TacoCloudApplicationTests {
 
 	@Test
 	void contextLoads() {
